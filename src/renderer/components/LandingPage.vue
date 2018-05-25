c6db95e6164978625bd034790a459dad5757c90f
deborah <deborah.recourse@hotmail.fr>
2018-05-25 15:12:11 +0200

    :gem: add line numbering

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -3,29 +3,41 @@
     <div class="info">
       JSON verify
     </div>
-    <textarea v-model="myjson" type="text" placeholder="Place your JSON here">
-    </textarea>
-    <select v-model="selected">
-      <option disabled value="">Spaces for a tab</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-    </select>
-    <button v-on:click="check" v-if="selected">Test my JSON</button>
     <div>
-      {{ errortext }}
+      <div class="numero" id="myscroll">
+        0 <br>
+        <span v-for="line in lines"> {{line}} <br> </span>
+        <br>
+      </div>
+      <textarea v-model="myjson" type="text" placeholder="Place your JSON here"
+      name="text" id="text" cols="30" v-on:keyup="keymonitor" v-on:keydown="keymonitor">
+      </textarea>
     </div>
+    <div class="input">
+      <select v-model="selected">
+        <option disabled value="">Spaces for a tab</option>
+        <option>2</option>
+        <option>3</option>
+        <option>4</option>
+        <option>5</option>
+      </select>
+      <button v-on:click="check" v-if="selected">Test my JSON</button>
+      </div>
+      <div v-if="error" class='error'>
+        {{ errortext }}
+      </div>
   </div>
 </template>
 
-<script>
-
+<script type="text/javascript">
 let data = {
   myjson: '',
   errortext: '',
-  selected: 0
+  selected: 0,
+  lines: 0,
+  error: false
 }
+
 export default ({
   name: 'landingpage',
   data: function () {
@@ -37,12 +49,25 @@ export default ({
         JSON.parse(this.myjson)
       } catch (err) {
         if (err) {
+          this.error = true
           this.errortext = '' + err
           return
         }
       }
+      this.error = false
       this.errortext = ''
       this.myjson = JSON.stringify(JSON.parse(this.myjson), null, parseInt(this.selected, 10))
+    },
+    keymonitor: function (event) {
+      var text = document.getElementById('text')
+      var scroll = document.getElementById('myscroll')
+      if (text.value.match(/[\r\n]/g)) {
+        this.lines = text.value.match(/[\r\n]/g).length
+      } else {
+        this.lines = 0
+      }
+      scroll.scrollTop = text.scrollTop + 2
+      console.log(text.scrollTop)
     }
   }
 })
@@ -67,26 +92,57 @@ export default ({
     padding:3px;
     border:1px solid rgba(0, 102, 255, 0.514);
     border-block-start-color: blue;
-    border-radius:5px;
     width:500px;
-    height:600px;
+    height:601px;
     word-wrap: physical;
     resize: none;
     overflow-y: hidden;
+    display:inline-block;
+    margin-left: -10px;
+    font-size: 16px;
+    line-height: 20px;
+    font-family: 'Source Sans Pro', sans-serif;
   }
 
-   textarea:focus {
+  textarea:focus {
     caret-color: #21abfa;
     outline: none;
-    box-shadow: 0 0 0 2pt rgb(27, 88, 202);
     overflow-y: visible;
-    
   }
 
   #wrapper {
     height: 100vh;
     padding: 60px 80px;
     width: 100vw;
+  }
+
+
+  .numero {
+    font-family: 'Source Sans Pro', sans-serif;
+    padding-top: 4px;
+    background-color: rgb(195, 198, 211);
+    color: rgb(99, 51, 187);
+    width:45px;
+    height: 605px;
+    float: left;
+    padding-left: 25px;
+    font-size: 16px;
+    line-height: 20px;
+    overflow: hidden;
+    overflow-y: visible;    
+  }
+
+  .error {
+    margin-top: 10px;
+    color: rgb(0, 0, 0);
+    padding: 5px;
+    border:2px solid rgb(255, 0, 0);
+    background: rgba(255, 0, 0, 0.678);
+    border-radius:5px;
+  }
+
+  .input {
+    margin-top: 20px;
   }
 
 </style>
